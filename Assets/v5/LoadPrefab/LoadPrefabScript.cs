b38d6fb34390ea6ab99968ba3bc4bc3516185946
Sevdat <55320710+Sevdat@users.noreply.github.com>
2025-03-23 00:07:21 +0100

    Update LoadPrefabScript.cs

--- Assets/v5/LoadPrefab/LoadPrefabScript.cs
+++ Assets/v5/LoadPrefab/LoadPrefabScript.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
+using UnityEditor;
 using System.Text.RegularExpressions;
+using UnityEditor.Experimental.GraphView;
 using System.IO;
 using System.Collections.Generic;
 
@@ -10,23 +12,43 @@ public class LoadPrefabScript : MonoBehaviour
     // public string prefabName = "ImportedScenes/Medieval Cute Series/Medieval Cute Build In 1";
     FolderPaths folderPaths;
     List<string> allScenePrefabs;
-    
 
     public void Awake(){
-        
-
+        // allScenePrefabs = getAllPrefabs();
     }
     public void Start(){
         folderPaths = transform.GetComponent<FolderPaths>();
-        
+        string[] lol = getPrefabsInFolder("Assets/ImportedAssets");
+        foreach (string i in lol) print(i);
     }
 
-    public string relativePath(string path){
-        return Path.Combine(Application.persistentDataPath, path);
-    }
     public static string getRelativeResourcePath(string fullPath){
         string pattern = @"(?<=Resources\/).+(?=\.prefab)";
         return Regex.Match(fullPath, pattern).Value;
+    }
+    public string[] getPrefabsGUIDInFolder(string folderPath){
+        return AssetDatabase.FindAssets("t:Prefab", new[] { folderPath });
+    }
+    public string[] getPrefabsInFolder(string folderPath){
+        string[] prefabGUIDs = getPrefabsGUIDInFolder(folderPath);
+        print(folderPath);
+        for (int i = 0; i<prefabGUIDs.Length;i++){
+            prefabGUIDs[i] = AssetDatabase.GUIDToAssetPath(prefabGUIDs[i]).Split(new [] {folderPath + "/"}, System.StringSplitOptions.None)[1];
+        }
+        return prefabGUIDs;
+    }
+    public List<string> getAllPrefabs(){
+        string folderInResources = "ImportedScenes";
+        string[] folders = Directory.GetDirectories(@$"C:\Users\sevda\Documents\GitHub\Museum-of-Questions\Assets\Resources\{folderInResources}");
+        List<string> paths = new List<string>();
+        for (int i = 0; i<folders.Length;i++){
+            string[] str = folders[i].Split("\\");
+            folders[i] = @$"Assets\Resources\{folderInResources}\" + folders[i].Split("\\")[str.Length-1];
+        }
+        foreach(string str in folders){
+            paths.AddRange(getPrefabsInFolder(str));
+        }
+        return paths;
     }
 
     internal IEnumerator LoadAndInstantiatePrefab(GameObject destroyPortal,Vector3 vec){
@@ -35,8 +57,9 @@ public class LoadPrefabScript : MonoBehaviour
         // Wait until the prefab is fully loaded
         yield return request;
         // Check if the prefab was loaded successfully
-        if (request.asset == null) yield break;
-        
+        if (request.asset == null){
+            yield break;
+        }
         // Instantiate the prefab into the scene
         GameObject root = request.asset as GameObject;
         if (root != null){
@@ -50,11 +73,14 @@ public class LoadPrefabScript : MonoBehaviour
         // Wait until the prefab is fully loaded
         yield return request;
         // Check if the prefab was loaded successfully
-        if (request.asset == null) yield break;
-
+        if (request.asset == null){
+            yield break;
+        }
         // Instantiate the prefab into the scene
         GameObject root = request.asset as GameObject;
-        if (root != null) folderPaths.currentMap = Instantiate(root, vec, Quaternion.identity);
+        if (root != null){
+            folderPaths.currentMap = Instantiate(root, vec, Quaternion.identity);
+        }
     }
     internal IEnumerator DeleteMapPrefab(GameObject prefab){
         // Destroy the last object
@@ -62,5 +88,4 @@ public class LoadPrefabScript : MonoBehaviour
         // Yield to the next frame to spread out the workload
         yield return null;
     }
-
 }
